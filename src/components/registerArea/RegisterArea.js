05b6af1f662d7e5f5e9a83b4802ad104a3075629
UtkarshKeerti <utkarsh.keerti@gmail.com>
2022-04-23 01:04:56 +0530

    photos added

--- src/components/registerArea/RegisterArea.js
+++ src/components/registerArea/RegisterArea.js
@@ -1,13 +1,14 @@
 import { SubtitlesOutlined } from '@mui/icons-material';
 import React from 'react';
 import styles from './registerArea.module.css';
+import ButtonCustom from '../buttonCustom/ButtonCustom';
 const RegisterArea = () => {
   return (
     <div className={styles.registerAreaMother}>
       <div className={styles.DocRegisterArea}>
         <h3>ARE YOU A DOCTOR?</h3>
         <p>The service allows you to get maximum visibility online and to manage appointments and contacts coming from the site, in a simple and fast way.</p>
-        <button>Register Now</button>
+        <a href="/login"><ButtonCustom primaryBtn btnText="Primary Text" /></a>
       </div>
       <div className={styles.PatientRegisterArea}>
         <h3>ARE YOU A PATIENT</h3>
