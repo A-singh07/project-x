13f1197780ec1531a96b20bf4f02a26dcdbf9f61
Aman Singh <imaman07singh@gmail.com>
2022-04-23 00:34:06 +0530

    signup form fix

--- src/components/LoginSignupComp/SignupForm.js
+++ src/components/LoginSignupComp/SignupForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { usestate } from 'react'
 import styles from './LoginSignupComp.module.css'
 import ButtonCustom from '../buttonCustom/ButtonCustom'
 import CreateAccountImg from '../../assets/CreateAccount.jpg'
@@ -15,16 +15,16 @@ const SignupForm = () => {
   // }
   const options = [
     {
-      value: 'Doctor',
+      value: 'doctor',
       label: 'Doctor',
     },
     {
-      value: 'Patient',
+      value: 'patient',
       label: 'Patient',
     },
   ];
 
-  const [option, setOption] = React.useState('Doctor');
+  const [option, setOption] = useState('doctor');
 
   const handleChange = (event) => {
     setOption(event.target.value);
@@ -39,10 +39,7 @@ const SignupForm = () => {
       <div className={styles.LSInner}>
         <div className={styles.LSLeft}>
           <form action="">
-            <div id={styles.names} className={styles.NameInputCont}>
-              <TextField id="standard-basic" label="First Name" fullWidth />
-              <TextField id="standard-basic" label="Last Name" fullWidth />
-            </div>
+            <TextField style={{ marginBottom: "10px" }} id="standard-basic" label="Full Name" fullWidth />
             {/* <select name="" id="">
               <option id={styles.disabled} value="Select" disabled selected hidden>Select</option>
               <option value="Doctor">Doctor</option>
@@ -51,12 +48,11 @@ const SignupForm = () => {
             <TextField
               style={{ marginBottom: "10px" }}
               fullWidth
-              id="outlined-select-currency"
+              id="outlined-select-user"
               select
               label="Select"
               value={option}
               onChange={handleChange}
-            // helperText="Please select your currency"
             >
               {options.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
@@ -65,9 +61,15 @@ const SignupForm = () => {
               ))}
             </TextField>
             <TextField style={{ marginBottom: "10px" }} id="standard-basic" label="Email" fullWidth />
+            {
+              option === 'doctor' &&
+              <>
+                <TextField style={{ marginBottom: "10px" }} id="standard-basic" label="License Number" fullWidth />
+                <TextField style={{ marginBottom: "10px" }} id="standard-basic" label="Speciality" fullWidth />
+              </>
+            }
             <TextField style={{ marginBottom: "10px" }} id="standard-basic" label="Password" fullWidth />
             <TextField style={{ marginBottom: "10px" }} id="standard-basic" label="Confirm Password" fullWidth />
-            {/* <input id={styles.CPass} type="text" placeholder="Confirm Password" /> */}
           </form>
           <div className={styles.CABtn}>
             <ButtonCustom primaryBtn btnText="Create Account" />
